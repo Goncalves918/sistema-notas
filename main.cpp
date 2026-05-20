60d5d67d9363ada6abd7fff7a36897f93a5b9174
victor <victor.d.silva@edu.senai.br>
2026-05-20 10:28:26 -0300

    feat: adiciona notas e cálculo de média

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@ int main () {
     // Entrada
         string nomes [20];
         int qtdAlunos;
+        float notas [20][4];
+        float media [20];
+        int qtdDisciplina;
 
 
 
@@ -24,6 +27,38 @@ int main () {
         cout << "Digite o nome do aluno " << (i + 1) << ": ";
         getline(cin, nomes[i]);
     }
+
+
+     do {
+        cout << "Quantidade de disciplinas (1 a 4): ";
+        cin >> qtdDisciplina;
+        }while (qtdDisciplina < 1 || qtdDisciplina > 4);
+
+        for (int i = 0; i < qtdAlunos; i++) {
+            cout << "Notas do aluno " << nomes[i] << ":" << endl;
+            for (int j = 0; j < qtdDisciplina; j++) {
+                cout << "Disciplina " << (j + 1) << ": ";
+                cin >> notas[i][j];
+            while (notas[i][j] < 0 || notas[i][j] > 10) {
+                cout << "Nota inválida. Digite novamente: ";
+                cin >> notas[i][j];
+            }
+            }
+     
+            
+        }
+
+         for (int i = 0; i < qtdAlunos; i++) {
+            float soma = 0;
+            for (int j = 0; j < qtdDisciplina; j++) {
+                soma += notas[i][j];
+            }
+            media[i] = soma / qtdDisciplina;
+        }
+
+
+
+
 
 
     //Saída
